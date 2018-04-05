5f9c5b9e9f6f5f3bc0c0c2d5129356195b2dca99
Albert Kaaman <albert@kaaman.nu>
2018-04-05 08:23:31 +0200

    fix: import reference to removed component

--- components/translation/index.js
+++ components/translation/index.js
@@ -1,10 +1,8 @@
-import TranslateBtn from './TranslateBtn'
 import Translatable from './Translatable'
 import TranslationBar from './TranslationBar'
 import CreateTranslationDialog from './CreateTranslationDialog'
 
 export default {
-  TranslateBtn,
   TranslationBar,
   Translatable,
   CreateTranslationDialog
