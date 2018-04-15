0e32bd2425a015f3888d6e21a91099422351f4f9
nekosaur <albert@kaaman.nu>
2018-04-15 18:49:41 +0200

    fix: no need to render translation stuff if were not translating

--- components/core/AppView.vue
+++ components/core/AppView.vue
@@ -3,17 +3,23 @@
     v-fade-transition(mode="out-in")
       router-view(:key="$route.path")
     //- app-footer
-    translation-bar
-    create-translation-dialog
+    translation-bar(v-if="isTranslating")
+    create-translation-dialog(v-if="isTranslating")
 </template>
 
 <script>
+  import { mapState } from 'vuex'
   // Components
   import AppFooter from '@/components/core/AppFooter'
 
   export default {
     components: {
       AppFooter
+    },
+    computed: {
+      ...mapState('translation', {
+        isTranslating: state => state.isTranslating
+      })
     }
   }
 </script>
